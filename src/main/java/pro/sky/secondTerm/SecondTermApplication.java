396ae7ebf8e4daa435d188df2874f050a1deef94
shard <alisher.sharipov2@mail.ru>
2023-06-10 21:57:37 +0600

    Initial Push

---
+++ src/main/java/pro/sky/secondTerm/SecondTermApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.secondTerm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondTermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondTermApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/secondTerm/SecondTermApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.secondTerm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondTermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

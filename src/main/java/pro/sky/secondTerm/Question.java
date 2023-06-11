4e5c10ccc4ed4a44d09931f09e1c7cf995fa2a38
shard <alisher.sharipov2@mail.ru>
2023-06-12 01:23:29 +0600

    Basic difficulty

---
+++ src/main/java/pro/sky/secondTerm/Question.java
@@ -0,0 +1,38 @@
+package pro.sky.secondTerm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class Question {
+    private String question;
+    private String answer;
+    public Question() {}
+    public Question(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+    public String getQuestion() {
+        return question;
+    }
+    public String getAnswer() {
+        return answer;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Question)) return false;
+        Question question1 = (Question) o;
+        return Objects.equals(question, question1.question) && Objects.equals(answer, question1.answer);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(question, answer);
+    }
+    @Override
+    public String toString() {
+        return "question = " + question + '\'' + ", answer = " + answer;
+    }
+}
---
+++ src/test/java/pro/sky/secondTerm/ConstantsForTests.java
@@ -0,0 +1,11 @@
+package pro.sky.secondTerm;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class ConstantsForTests {
+    public static final Question firstQuestion = new Question("Вопрос 1", "Ответ 1");
+    public static final Question secondQuestion = new Question("Вопрос 2", "Ответ 2");
+    public static final Question thirdQuestion = new Question("Вопрос 3", "Ответ 3");
+    public static final Collection<Question> questionsForTests = Set.of(firstQuestion, secondQuestion, thirdQuestion);
+}
